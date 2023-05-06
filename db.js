c65afd8cb8b0aad64a71de26c262ca4431db9e1a
Ilya <iponomarev@dengisrazy.ru>
2023-05-06 23:38:11 +0400

    db

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require("sequelize");
 
 module.exports = new Sequelize("ka1h2h", "root", "root", {
-  host: "45.89.27.95",
+  host: "192.168.0.84",
   port: "6432",
   dialect: "postgres",
 });
